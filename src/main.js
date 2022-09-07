2628539c499a5f1a8f739770d7667b0f44a0baf7
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-07 01:16:52 -0300

    chore: add husky and lint-staged

--- src/main.js
+++
@@ -1,9 +0,0 @@
-console.log(1)
-console.log('skjdhfpsdfnasiduf')
-const comma = []
-const bahiano = 'sdhfgasdlfh'
-
-if (comma === bahiano) {
-  const trade = 'sjudfhs'
-  console.log(trade)
-}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log(12);
+console.log(12)
+
+
+
+console.log(1)
+console.log('skjdhfpsdfnasiduf')
+
+
+
+const comma = []
+const bahiano = ['sdhfgasdlfh']
+
+if (comma === bahiano) {
+  const trade = 'sjudfhs'
+
+  
+  console.log(trade)
+}
