24cd50ec055a4bcc95428c8bcc6acad52f3cfd0b
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-10 13:05:40 -0300

    feat: ensure SignupController validates email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,12 +2,17 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { badRequest } from '../helpers/http-helpers'
 import { IController } from '../protocols/controller'
+import { IEmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 //name, email, password, passwordConfirmation
 ////cargos e senioridades serão definidos ao entrar na empresa
 
 export class SignupController implements IController {
+  private readonly emailValidator: IEmailValidator
+  constructor(emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator
+  }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
@@ -18,6 +23,10 @@ export class SignupController implements IController {
     const { name, email, password, passwordConfirmation } = httpRequest.body
     if (password !== passwordConfirmation) {
       return badRequest(new InvalidParamError('passwordConfirmation'))
+    }
+    const isValidEmail = this.emailValidator.isValid(email)
+    if (!isValidEmail) {
+      return badRequest(new InvalidParamError('email'))
     }
 
     const httpResponse: HttpResponse = {
---
+++ src/utils/email-validator-adapter.ts
@@ -0,0 +1,8 @@
+import validator from 'validator'
+import { IEmailValidator } from '../presentation/protocols/email-validator'
+
+export class EmailValidatorAdapter implements IEmailValidator {
+  isValid(email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
