9a9d98d5ff567192dca40a7f98d00e621ed92ad2
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-11 16:14:01 -0300

    refactor: add method to id builder

--- src/data/protocols/id-builder.ts
+++ src/data/protocols/id-builder.ts
@@ -1,3 +1,4 @@
 export interface IDBuilder {
   createId: () => string
+  isUUID: (value: string) => boolean
 }
--- src/utils/uuid-adapter.ts
+++ src/utils/uuid-adapter.ts
@@ -1,8 +1,12 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid'
 import { IDBuilder } from '../data/protocols/id-builder'
 
 export class UUIDBuilder implements IDBuilder {
   createId(): string {
     return uuidv4()
+  }
+
+  isUUID(value: string): boolean {
+    return uuidValidate(value)
   }
 }
