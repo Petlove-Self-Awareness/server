c5b08998372fb61334803dcc3dc7a66f6d603b8c
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-19 22:00:33 -0300

    test: compose Signup use case and controller

---
+++ src/main/factories/controllers/user/singup/signup-validation.spec.ts
@@ -0,0 +1,26 @@
+import { makeSignUpValidation } from './signup-validation-factory'
+import {
+  RequiredFieldValidation,
+  EmailValidation,
+  ValidationComposite,
+  CompareFieldsValidation
+} from '../../../../../presentation/helpers/validators'
+import { IValidation } from '../../../../../presentation/protocols/validation'
+import { EmailValidatorAdapter } from '../../../../../utils/email-validator-adapter'
+
+jest.mock('../../../../../presentation/helpers/validators/validation-composite')
+
+describe('SignUpValidation Factory', () => {
+  test('Should call ValidationComposite with all validations ', () => {
+    makeSignUpValidation()
+    const validations: IValidation[] = []
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    validations.push(
+      new CompareFieldsValidation('password', 'passwordConfirmation')
+    )
+    validations.push(new EmailValidation(new EmailValidatorAdapter(), 'email'))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
