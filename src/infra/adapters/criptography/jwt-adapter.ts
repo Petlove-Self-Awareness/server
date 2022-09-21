fc29c1f6766325a9224d6a5bbaaa136a41c145b8
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-20 21:12:13 -0300

    feat: implement jwt adapter

---
+++ src/infra/adapters/criptography/jwt-adapter.ts
@@ -0,0 +1,9 @@
+import { IEncrypter } from '../../../data/protocols/criptography'
+import jwt from 'jsonwebtoken'
+
+export class JwtAdapter implements IEncrypter {
+  encrypt(id: string): string {
+    const token = jwt.sign(id, process.env.JWT_SECRET || 'petloveServerPass')
+    return token
+  }
+}
