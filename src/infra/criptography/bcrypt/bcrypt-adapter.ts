0796105f01d5775ae8e816696046fd67c267fed0
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-09-20 22:12:50 -0300

    feat: add compare to BcryptAdapter

--- src/infra/criptography/bcrypt/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt/bcrypt-adapter.ts
@@ -1,12 +1,18 @@
 import * as bcrypt from 'bcryptjs'
+import { IHashComparer } from '../../../data/protocols/criptography'
 import { IHasher } from '../../../data/protocols/criptography/hasher'
 
-export class BcryptAdapter implements IHasher {
+export class BcryptAdapter implements IHasher, IHashComparer {
   private salt: number
   constructor(salt: number) {
     this.salt = salt
   }
   async hash(value: string): Promise<string> {
     return bcrypt.hash(value, this.salt)
+  }
+
+  async compare(value: string, hashedValue: string): Promise<boolean> {
+    const compare = await bcrypt.compare(value, hashedValue)
+    return compare
   }
 }
