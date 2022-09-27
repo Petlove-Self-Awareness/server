bcd561914f3c69cbb1c1c1ad0e6f8b69b1c5a698
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-09-26 22:13:03 -0300

    fix: changing UdateUserDto data to optional

--- src/domain/usecases/user/update-user.ts
+++ src/domain/usecases/user/update-user.ts
@@ -2,10 +2,10 @@ import { Result } from '../../logic/result'
 import { IUserModel } from '../../models/user-model'
 
 export interface UpdateUserDto {
-  id: string;
-  name: string
-  email: string
-  password: string
+  id: string
+  name?: string
+  email?: string
+  password?: string
 }
 
 export interface IUpdateUserUseCase {
