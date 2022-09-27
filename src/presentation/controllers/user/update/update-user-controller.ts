da87969cc08c8b682cdb09efad556b3fd8f61302
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-09-26 22:14:06 -0300

    feat: ensure UpdateUserController calls Validation with correct values

--- src/presentation/controllers/user/update/update-user-controller.ts
+++ src/presentation/controllers/user/update/update-user-controller.ts
@@ -1,18 +1,29 @@
 import { IUpdateUserUseCase } from '../../../../domain/usecases/user/update-user'
 import {
+  badRequest,
   HttpRequest,
   HttpResponse,
-  IController
+  IController,
+  IValidation
 } from '../login/login-controller-protocols'
 
 export class UpdateUserController implements IController {
-  constructor(private readonly userUpdateUseCase: IUpdateUserUseCase,) {}
+  constructor(
+    private readonly validation: IValidation,
+    private readonly userUpdateUseCase: IUpdateUserUseCase
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const userRequest = Object.assign(httpRequest.body, {
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
+
+    const dataToUpdate = Object.assign(httpRequest.body, {
       id: httpRequest.accountId
     })
-    await this.userUpdateUseCase.update(userRequest)
+
+    await this.userUpdateUseCase.update(dataToUpdate)
     return null
   }
 }
