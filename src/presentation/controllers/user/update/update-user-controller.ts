1561441fc52643e8dc6bd873886c0f0f26cd222b
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-09-27 19:30:16 -0300

    feat: ensure return 500 if UserUpdateUseCase throws

--- src/presentation/controllers/user/update/update-user-controller.ts
+++ src/presentation/controllers/user/update/update-user-controller.ts
@@ -6,33 +6,38 @@ import {
   IController,
   InvalidParamError,
   MissingParamError,
-  ok
+  ok,
+  serverError
 } from '../login/login-controller-protocols'
 
 export class UpdateUserController implements IController {
   constructor(private readonly userUpdateUseCase: IUpdateUserUseCase) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { name, email, password, passwordConfirmation } = httpRequest.body
-    if (!name && !email && !password) {
-      return badRequest(
-        new MissingParamError('name, email or password not informed')
-      )
-    }
-    if (password && !passwordConfirmation) {
-      return badRequest(
-        new MissingParamError('password confirmation not informed')
-      )
-    }
-    if (password !== passwordConfirmation) {
-      return badRequest(new InvalidParamError('passwordConfirmation'))
-    }
+    try {
+      const { name, email, password, passwordConfirmation } = httpRequest.body
+      if (!name && !email && !password) {
+        return badRequest(
+          new MissingParamError('name, email or password not informed')
+        )
+      }
+      if (password && !passwordConfirmation) {
+        return badRequest(
+          new MissingParamError('password confirmation not informed')
+        )
+      }
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
 
-    const dataToUpdate = Object.assign(httpRequest.body, {
-      id: httpRequest.accountId
-    })
+      const dataToUpdate = Object.assign(httpRequest.body, {
+        id: httpRequest.accountId
+      })
 
-    const updatedUser = await this.userUpdateUseCase.update(dataToUpdate)
-    return ok(updatedUser)
+      const updatedUser = await this.userUpdateUseCase.update(dataToUpdate)
+      return ok(updatedUser)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
